0678797ebab12392b0f9c5b0c1ad1f5e01800d9e
Alec Saunders <alecsaunders90@gmail.com>
2017-08-13 20:29:28 -0600

    Initial Commit

---
+++ Todo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Alec Saunders on 8/13/17.
+//  Copyright © 2017 Alec Saunders. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
