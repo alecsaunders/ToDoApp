1627458d29e1bf70604187f1dca7d0934880b1f2
Alec Saunders <alecsaunders90@gmail.com>
2017-08-19 16:05:19 -0600

    Moved all logic to Controller.swift

--- Todo/Controller.swift
+++ Todo/Controller.swift
@@ -10,11 +10,23 @@ import Foundation
 import Cocoa
 import CoreData
 
-class MainController: NSObject {
+
+protocol MainTableViewDelgate: class {
+    func reloadData()
+}
+
+class MainController: NSObject, NSTableViewDelegate, NSTableViewDataSource {
+    let registeredTypes:[String] = [NSGeneralPboard]
     let appDelegate = NSApplication.shared().delegate as? AppDelegate
     var managedContext: NSManagedObjectContext? = nil
     var coreDataToDoManagedObjects: [NSManagedObject]? = nil
     var mainTableToDoArray: [ToDo] = []
+    weak var mainTableViewDelgate: MainTableViewDelgate?
+    
+    fileprivate enum CellIdentifiers {
+        static let col_complete = "col_complete"
+        static let col_toDoText = "col_toDoText"
+    }
     
     override init() {
         super.init()
@@ -24,6 +36,7 @@ class MainController: NSObject {
         mainTableToDoArray = populateMainTableToDoArray()
     }
     
+    // MARK: - Core Data Setup
     func fetchManagedObjectsFromCoreData(entityName: String) -> [NSManagedObject]? {
         let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
         
@@ -55,18 +68,32 @@ class MainController: NSObject {
         return currentToDo
     }
     
-    func completedWasChecked(state: Int, btnIndex: Int) {
-        print(state)
-        print(btnIndex)
-        switch state {
-        case NSOnState:
-            removeToDoEntityRecord(atIndex: btnIndex)
-        case NSOffState:
-            break
-        default:
-            break
+    func save(currentToDoTitle: String) {
+        guard let mc = managedContext else { return }
+        
+        let entityName = "ToDo"
+        
+        let entity = NSEntityDescription.entity(forEntityName: entityName, in: mc)
+        let toDoEntityRecord = NSManagedObject(entity: entity!, insertInto: mc)
+        
+        
+        toDoEntityRecord.setValue(currentToDoTitle, forKeyPath: "title")
+        toDoEntityRecord.setValue(false, forKeyPath: "completed")
+        
+        let ordinalPosition = mainTableToDoArray.count
+        
+        toDoEntityRecord.setValue(ordinalPosition, forKey: "ordinalPosition")
+        
+        do {
+            try mc.save()
+            coreDataToDoManagedObjects = fetchManagedObjectsFromCoreData(entityName: entityName)
+            mainTableToDoArray.append(createToDoFromManagedObject(obj: toDoEntityRecord))
+            mainTableViewDelgate?.reloadData()
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
         }
     }
+
     
     func removeToDoEntityRecord(atIndex: Int) {
         guard let context = self.managedContext else { return }
@@ -76,33 +103,23 @@ class MainController: NSObject {
             try context.save()
             coreDataToDoManagedObjects!.remove(at: atIndex)
             mainTableToDoArray.remove(at: atIndex)
+            mainTableViewDelgate?.reloadData()
         } catch let error as NSError {
             print("Could not delete. \(error), \(error.userInfo)")
         }
     }
     
-    func save(currentToDoTitle: String) {
-        guard let mc = managedContext else { return }
-        
-        let entityName = "ToDo"
-        
-        let entity = NSEntityDescription.entity(forEntityName: entityName, in: mc)
-        let toDoEntityRecord = NSManagedObject(entity: entity!, insertInto: mc)
-
-
-        toDoEntityRecord.setValue(currentToDoTitle, forKeyPath: "title")
-        toDoEntityRecord.setValue(false, forKeyPath: "completed")
-
-        let ordinalPosition = mainTableToDoArray.count
-
-        toDoEntityRecord.setValue(ordinalPosition, forKey: "ordinalPosition")
-
-        do {
-            try mc.save()
-            coreDataToDoManagedObjects = fetchManagedObjectsFromCoreData(entityName: entityName)
-            mainTableToDoArray.append(createToDoFromManagedObject(obj: toDoEntityRecord))
-        } catch let error as NSError {
-            print("Could not save. \(error), \(error.userInfo)")
+    
+    // MARK: - Update View
+    func completedWasChecked(state: Int, btnIndex: Int) {
+        switch state {
+        case NSOnState:
+            removeToDoEntityRecord(atIndex: btnIndex)
+            mainTableViewDelgate?.reloadData()
+        case NSOffState:
+            break
+        default:
+            break
         }
     }
     
@@ -127,9 +144,70 @@ class MainController: NSObject {
 
         do {
             try mc.save()
+            print("MC saved")
             coreDataToDoManagedObjects = fetchManagedObjectsFromCoreData(entityName: "ToDo")
+            mainTableViewDelgate?.reloadData()
         } catch let error as NSError {
             print("Could not save. \(error), \(error.userInfo)")
         }
+    }
+    
+    // MARK: - Table View Methods
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return mainTableToDoArray.count
+    }
+    
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let theToDo = mainTableToDoArray[row]
+        
+        var cellIdentifier: String = ""
+        
+        var cell: NSTableCellView? = nil
+        
+        if tableColumn == tableView.tableColumns[0] {
+            cellIdentifier = CellIdentifiers.col_complete
+            cell = tableView.make(withIdentifier: cellIdentifier, owner: nil) as? NSTableCellView
+            if let completedCheck = cell?.subviews[0] as? NSButton {
+                completedCheck.tag = row
+                completedCheck.state = theToDo.completed ? 1 : 0
+            }
+        } else if tableColumn == tableView.tableColumns[1] {
+            cellIdentifier = CellIdentifiers.col_toDoText
+            cell = tableView.make(withIdentifier: cellIdentifier, owner: nil) as? NSTableCellView
+            cell?.textField?.stringValue = theToDo.title
+        }
+        
+        return cell!
+    }
+    
+    func doubleClickMainTVCell(sender: AnyObject) {
+        guard let tv = sender as? NSTableView else { return }
+        print(mainTableToDoArray[tv.selectedRow])
+    }
+    
+    func tableView(_ tableView: NSTableView, validateDrop info: NSDraggingInfo, proposedRow row: Int, proposedDropOperation dropOperation: NSTableViewDropOperation) -> NSDragOperation {
+        if dropOperation == .above {
+            return .move
+        }
+        return .every
+    }
+    
+    func tableView(_ tableView: NSTableView, writeRowsWith rowIndexes: IndexSet, to pboard: NSPasteboard) -> Bool {
+        let data = NSKeyedArchiver.archivedData(withRootObject: rowIndexes)
+        pboard.declareTypes(registeredTypes, owner: self)
+        pboard.setData(data, forType: NSGeneralPboard)
+        return true
+    }
+    
+    func tableView(_ tableView: NSTableView, acceptDrop info: NSDraggingInfo, row: Int, dropOperation: NSTableViewDropOperation) -> Bool {
+        let dragData = info.draggingPasteboard().data(forType: NSGeneralPboard)!
+        let rowIndexes: IndexSet? = NSKeyedUnarchiver.unarchiveObject(with: dragData) as? IndexSet
+        guard let ri: IndexSet = rowIndexes else { return true }
+        let dragOrigin = ri.first!
+        let dragDest = row
+        reorderToDos(dragOrigin: dragOrigin, dragDest: dragDest)
+        mainTableViewDelgate?.reloadData()
+        return true
     }
 }
--- Todo/MainWindowVC.swift
+++ Todo/MainWindowVC.swift
@@ -9,110 +9,36 @@
 import Cocoa
 import CoreData
 
-class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
+
+class ViewController: NSViewController, MainTableViewDelgate {
     @IBOutlet var mainTableView: NSTableView!
     @IBOutlet var textAddToDo: NSTextField!
     @IBAction func btnAdd(_ sender: NSButton) {
         let txt = textAddToDo.stringValue
         cntlr.save(currentToDoTitle: txt)
         textAddToDo.stringValue = ""
-        mainTableView.reloadData()
     }
     @IBAction func completedCheck(_ sender: NSButton) {
         cntlr.completedWasChecked(state: sender.state, btnIndex: sender.tag)
-        mainTableView.reloadData()
     }
-    
-    fileprivate enum CellIdentifiers {
-        static let col_complete = "col_complete"
-        static let col_toDoText = "col_toDoText"
-    }
-    
-    let registeredTypes:[String] = [NSGeneralPboard]
+
     let cntlr = MainController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mainTableView.delegate = self
-        mainTableView.dataSource = self
+        cntlr.mainTableViewDelgate = self
+        mainTableView.delegate = cntlr
+        mainTableView.dataSource = cntlr
         mainTableView.usesAlternatingRowBackgroundColors = true
         mainTableView.setDraggingSourceOperationMask(NSDragOperation.every, forLocal: false)
-        mainTableView.register(forDraggedTypes: registeredTypes)
-        mainTableView.doubleAction = #selector(self.doubleClickMainTVCell)
+        mainTableView.register(forDraggedTypes: cntlr.registeredTypes)
+        mainTableView.doubleAction = #selector(cntlr.doubleClickMainTVCell)
         mainTableView.reloadData()
     }
     
-    func numberOfRows(in tableView: NSTableView) -> Int {
-        return cntlr.mainTableToDoArray.count
-    }
     
-    
-    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        let theToDo = cntlr.mainTableToDoArray[row]
-        
-        var cellIdentifier: String = ""
-        
-        var cell: NSTableCellView? = nil
-        
-        if tableColumn == tableView.tableColumns[0] {
-            cellIdentifier = CellIdentifiers.col_complete
-            cell = tableView.make(withIdentifier: cellIdentifier, owner: nil) as? NSTableCellView
-            if let completedCheck = cell?.subviews[0] as? NSButton {
-                completedCheck.tag = row
-                completedCheck.state = theToDo.completed ? 1 : 0
-            }
-        } else if tableColumn == tableView.tableColumns[1] {
-            cellIdentifier = CellIdentifiers.col_toDoText
-            cell = tableView.make(withIdentifier: cellIdentifier, owner: nil) as? NSTableCellView
-            cell?.textField?.stringValue = theToDo.title
-        }
-        
-        return cell!
-    }
-    
-    func doubleClickMainTVCell(sender: AnyObject) {
-        guard let tv = sender as? NSTableView else { return }
-        print(cntlr.mainTableToDoArray[tv.selectedRow])
-    }
-    
-    func tableView(_ tableView: NSTableView, validateDrop info: NSDraggingInfo, proposedRow row: Int, proposedDropOperation dropOperation: NSTableViewDropOperation) -> NSDragOperation {
-        if dropOperation == .above {
-            return .move
-        }
-        return .every
-    }
-    
-    func tableView(_ tableView: NSTableView, writeRowsWith rowIndexes: IndexSet, to pboard: NSPasteboard) -> Bool {
-        let data = NSKeyedArchiver.archivedData(withRootObject: rowIndexes)
-        pboard.declareTypes(registeredTypes, owner: self)
-        pboard.setData(data, forType: NSGeneralPboard)
-        return true
-    }
-    
-    func tableView(_ tableView: NSTableView, acceptDrop info: NSDraggingInfo, row: Int, dropOperation: NSTableViewDropOperation) -> Bool {
-        let dragData = info.draggingPasteboard().data(forType: NSGeneralPboard)!
-        let rowIndexes: IndexSet? = NSKeyedUnarchiver.unarchiveObject(with: dragData) as? IndexSet
-        guard let ri: IndexSet = rowIndexes else { return true }
-        let dragOrigin = ri.first!
-        let dragDest = row
-        cntlr.reorderToDos(dragOrigin: dragOrigin, dragDest: dragDest)
+    func reloadData() {
         mainTableView.reloadData()
-        return true
-    }
-    
-    
-    func pressToDoBtn() {
-        if !textAddToDo.stringValue.isEmpty {
-            //save(currentToDoTitle: textAddToDo.stringValue)
-            clearTextAddToDo()
-            mainTableView.reloadData()
-        }
-    }
-
-    func clearTextAddToDo() {
-        textAddToDo.stringValue = ""
-        textAddToDo.becomeFirstResponder()
     }
 }
-
