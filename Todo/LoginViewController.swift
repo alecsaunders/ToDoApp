c66196b4398adc1038afc98710d37f1ddbe7fbd4
Alec <asaunders@admarketplace.com>
2018-06-26 12:36:43 -0400

    Add Login View controller

---
+++ Todo/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  Todo
+//
+//  Created by Alec Saunders on 6/26/18.
+//  Copyright © 2018 Alec Saunders. All rights reserved.
+//
+
+import Cocoa
+
+class LoginViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
